d3642951483f277c5861b73415d15e08204dc45e
WiBla <contact.wibla@gmail.com>
2015-09-24 18:06:27 +0200

    A new beginning.
    This script can:
    - Auto dub
    - Custom general appearance
    - "Minify" the chat
    - Prevent yourself to accidentally quit or refresh the page
    - Kill itself if needed
    
    Enjoy ! :confetti_ball:

--- ressources/script.js
+++ ressources/script.js
@@ -34,10 +34,12 @@ if(!$("#fcs-css")[0]) {
 			"advance": $(this).trigger("fcs:advance"),
 			"woot": $(this).trigger("fcs:woot"),
 			"meh": $(this).trigger("fcs:meh"),
+			"user_join": $(this).trigger("fcs:user_join"),
+			"user_leave": $(this).trigger("fcs:user_leave"),
 		},
 		"items": {
 			"script": {
-				"menu" : $("#fcs-menu")[0]
+				
 			},
 			"dubUp": $(".dubup"),
 			"dubDown": $(".dubdown")
@@ -54,7 +56,7 @@ if(!$("#fcs-css")[0]) {
 								<img src="https://api.dubtrack.fm/user/55ffc26f1564a403003f527e/image" alt="wibla" onclick="Dubtrack.helpers.displayUser(\'55ffc26f1564a403003f527e\', this);" class="cursor-pointer" onerror="Dubtrack.helpers.image.imageError(this);">\
 							</div>\
 							<div class="activity-row">\
-								<div class="text"><p><a href="#" class="username">FCS</a>'+msg+'</p>\
+								<div class="text"><p><a href="#" class="username">[FCS] </a>'+msg+'</p>\
 							</div>\
 							<div class="meta-info">\
 								<span class="username">wibla </span>\
@@ -88,115 +90,158 @@ if(!$("#fcs-css")[0]) {
 		"paintGreen": function(e) {
 			e[0].className = "on";
 		},
-		"paintOrange": function() {
+		"paintOrange": function(e) {
 			e[0].className = "off";
 		},
+		"getTimeRemaining": function() {
+			return $(".min")[0].innerHTML + ":" + $(".sec")[0].innerHTML;
+		},
+		"getVolume": function() {
+			return parseInt($(".volume .ui-slider-handle")[0].style.left);
+		},
+		"setVolume": function() {},
 			// menu
 		"autoVote": function() {
 			if (fcs.settings.autoVote) {
-				if (!fcs.item.dubUp.hasClass("voted") || !fcs.items.dubDown.hasClass("voted")) fcs.item.dubUp.click();
+				var woot = fcs.items.dubUp,
+				    meh = fcs.items.dubDown;
+				if (!woot.hasClass("voted") || !meh.hasClass("voted")) woot.click();
 			}
 		},
-		"theme": function(){},
-		"smallChat": function(){},
+		"theme": function(){
+			if (fcs.user.settings.theme) {
+				$("head").append($("<link id='fcs-theme-css' rel='stylesheet' type='text/css' href='"+fcs.theme_css+"'>"));
+			} else {
+				$("#fcs-theme-css").remove();
+			}
+		},
+		"smallChat": function(){
+			if (fcs.user.settings.smallChat) {
+				$("head").append($("<link id='fcs-smallChat-css' rel='stylesheet' type='text/css' href='"+fcs.smallChat_css+"'>"));
+			} else {
+				$("#fcs-smallChat-css").remove();
+			}
+		},
 		"kill": function() {
-			fcs.log("Aurevoir !", "log");
-			console.log("Aurevoir !");
-			init(false);
+			if (init(true) === "no problem") return "done";
 		}
 	};
 	
 	init();
+} else if (fcs.kill() === "done") {
+	setTimeout($.getScript("https://rawgit.com/WiBla/FCS/master/ressources/script.js"), 1000);
 }
 
 function init(kill) {
-	// Loading script's core elements
-	$("#header-global .user-info ul > li.user-messages").before(
-	$('<li id="fcs-logo">\
-			<button id="fcs-button">FCS</button>\
-			<ul id="fcs-menu" style="display: none;">\
-				<li id="autoVote">Auto-Vote</li>\
-				<li id="theme">Theme</li>\
-				<li id="smallChat">Small chat</li>\
-				<li id="confirmQuit">Confirm on quit</li>\
-				<li id="kill">Shutdown</li>\
-			</ul>\
-		</li>'));
-	// Initating custom css
-	$("head").append($("<link id='fcs-css' rel='stylesheet' type='text/css' href='"+fcs.menu_css+"'>"));
-	// Getting settings
-	if (!localStorage.getItem("fcs-settings")) {
-		localStorage.setItem("fcs-settings", fcs.settings);
-		fcs.user.settings = fcs.settings;
-	} else {
-		fcs.user.settings = localStorage.getItem("fcs-settings");
-		var i=0,j=0;
-		for (var key in fcs.settings) {
-			i++;
-			for (var key2 in fcs.user.settings) {
-				j++;
-			}
-			if (i > j) {
-				fcs.user.settings[key2] = fcs.settings[key2];
-			}
-		}
-	}
-	fcs.autoVote();fcs.theme();fcs.smallChat();
-		
-	// ##### [Event handlers] #####
-	// menu's slide
-	$("#fcs-logo").on("click", function(){
-		var menu = fcs.items.script.menu;
-		if (menu.style.display === "none") {
-			menu.style.display = "block";
-			menu.style.height = "auto";
+	if (!kill) {
+		// Loading script's core elements
+		$("#header-global .user-info ul > li.user-messages").before(
+		$('<li id="fcs-logo">\
+				<button id="fcs-button">FCS</button>\
+				<ul id="fcs-menu" style="display: none;">\
+					<li id="autoVote">Auto-Vote</li>\
+					<li id="theme">Theme</li>\
+					<li id="smallChat">Small chat</li>\
+					<li id="confirmQuit">Confirm on quit</li>\
+					<li id="kill">Shutdown</li>\
+				</ul>\
+			</li>'));
+		// Initating custom css
+		$("head").append($("<link id='fcs-css' rel='stylesheet' type='text/css' href='"+fcs.menu_css+"'>"));
+		// Initating script's element as they cannot be reached until created
+		fcs.items.script = {
+			"menu": $("#fcs-menu")[0],
+		};
+		// Getting settings
+		if (!localStorage.getItem("fcs-settings")) {
+			localStorage.setItem("fcs-settings", JSON.stringify(fcs.settings));
+			fcs.user.settings = fcs.settings;
 		} else {
-			menu.style.display = "none";
-			menu.style.height = "0";
+			fcs.user.settings = JSON.parse(localStorage.getItem("fcs-settings"));
+			var i=0,j=0;
+			for (var key in fcs.settings) {
+				i++;
+				for (var key2 in fcs.user.settings) {
+					j++;
+				}
+				if (i > j) {
+					fcs.user.settings[key2] = fcs.settings[key2];
+				}
+			}
 		}
-	});
-	// menu selection
-	$("#fcs-menu").on("click", "li", function(){
-		var settings = fcs.user.settings;
-		switch($(this).id) {
-			case "autoVote":
-				settings.autoVote = !settings.autoVote;
-				if (settings.autoVote) fcs.paintGreen($(this));
-				else fcs.paintOrange($(this));
-				fcs.autoVote();
-			break;
-			case "theme":
-				settings.theme = !settings.theme;
-				if (settings.theme) fcs.paintGreen($(this));
-				else fcs.paintOrange($(this));
-				fcs.theme();
-			break;
-			case "smallChat":
-				settings.smallChat = !settings.smallChat;
-				if (settings.smallChat) fcs.paintGreen($(this));
-				else fcs.paintOrange($(this));
-				fcs.smallChat();
-			break;
-			case "confirmQuit":
-				settings.confirmQuit = !settings.confirmQuit;
-				if (settings.confirmQuit) fcs.paintGreen($(this));
-				else fcs.paintOrange($(this));
-			break;
-			case "kill":fcs.kill();break;
-			
-			default: console.log("Hey ! Yeah you WiBla, you f*ed up !");
-		}
-	});
-	$(".chat-main").on("DOMSubtreeModified", function() {
-	 // si fcs.item.chat.length et inférieur à $(this.length)
-	   // si chat.type = fcs-*
-	     // > fcs.event.chatLog(msg, type, time)
-	   // sinon fcs.event.chat(msg, sender, type, time)
-	});
-	window.onbeforeunload = function(){
-		if (fcs.user.settings.confirmQuit) return "Are you sure you want to quit ?";
-	};
-	
-	// loading complete
-	fcs.log("FCS $version loaded !", "log");
+		fcs.autoVote();fcs.theme();fcs.smallChat();
+
+		// ##### [Event Handlers] #####
+		// menu's slide
+		$("#fcs-logo").on("click", "button", function(){
+			var menu = fcs.items.script.menu;
+			if (menu.style.display === "none") {
+				menu.style.height = "auto";
+				setTimeout(function(){menu.style.display = "block";}, 0);
+			} else {
+				menu.style.height = "0";
+				setTimeout(function(){menu.style.display = "none";}, 0);
+			}
+		});
+		// menu selection
+		$("#fcs-menu").on("click", "li", function(){
+			var settings = fcs.user.settings;
+			switch($(this)[0].id) {
+				case "autoVote":
+					settings.autoVote = !settings.autoVote;
+					if (settings.autoVote) fcs.paintGreen($(this));
+					else fcs.paintOrange($(this));
+					fcs.autoVote();
+				break;
+				case "theme":
+					settings.theme = !settings.theme;
+					if (settings.theme) fcs.paintGreen($(this));
+					else fcs.paintOrange($(this));
+					fcs.theme();
+				break;
+				case "smallChat":
+					settings.smallChat = !settings.smallChat;
+					if (settings.smallChat) fcs.paintGreen($(this));
+					else fcs.paintOrange($(this));
+					fcs.smallChat();
+				break;
+				case "confirmQuit":
+					settings.confirmQuit = !settings.confirmQuit;
+					if (settings.confirmQuit) fcs.paintGreen($(this));
+					else fcs.paintOrange($(this));
+				break;
+				case "kill":fcs.kill();break;
+				
+				default: console.error("Hey ! Yeah you WiBla, you f*ed up !");
+			}
+			localStorage.setItem("fcs-settings", JSON.stringify(settings));
+		});
+		$(".chat-main").on("DOMSubtreeModified", function() {
+		 // si fcs.item.chat.length et inférieur à $(this.length)
+			 // si chat.type = fcs-*
+				 // > fcs.event.chatLog(msg, type, time)
+			 // sinon fcs.event.chat(msg, sender, type, time)
+		});
+		window.uInt = setInterval(fcs.autoVote, 1000); //this will be way better in the next update
+		window.onbeforeunload = function(){
+			if (fcs.user.settings.confirmQuit) return "Are you sure you want to quit ?";
+		};
+		
+		fcs.log(fcs.complete("$version loaded !"), "log");
+	} else {
+		// inversing functions
+			// if a function changes the DOM, invert-it to get the more basic dubtrack.fm possible
+		// Removing core elements
+		$("#fcs-logo").remove();
+		$("#fcs-css").remove();
+		// Unbinding Event Handlers
+		$("#fcs-logo").off();
+		$("#fcs-menu").off();
+		$(".chat-main").off();
+		clearInterval(window.uInt);
+		window.onbeforeunload = "";
+		// And finaly 
+		fcs.log("Aurevoir !", "log"); console.log("Aurevoir !");
+		return "no problem"; // needed in case of reload
+	}
 }
